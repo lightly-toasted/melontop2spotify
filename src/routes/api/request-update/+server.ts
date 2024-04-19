6a4d967527525e418b815a9d6ad542cc98db88c3
toast <toastlua@gmail.com>
2024-04-20 00:37:57 +0900

    use artist filter to improve track search results

--- src/routes/api/request-update/+server.ts
+++ src/routes/api/request-update/+server.ts
@@ -20,11 +20,8 @@ function getCurrentTimestamp() {
     return new Date().getTime() / 1000
 }
 
-const headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
-}
-
 async function updatePlaylist(name: string): Promise<UpdateResult> {
+    // update playlist descriptions
     kv.set('lastcheck', getCurrentTimestamp())
 
     const refreshData = await spotify.refreshAccessToken()
@@ -35,15 +32,27 @@ async function updatePlaylist(name: string): Promise<UpdateResult> {
 
     await spotify.changePlaylistDetails(env.PLAYLIST_ID, { name: newPlaylistName, description: newPlaylistDesc })
 
+    // crawl melon chart
+    const headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
+    }
+
     const response = await axios.get(env.MELON_URL, { headers })
 
     const root = parse(response.data);
-    const chart: string[] = [];
+    const chart: {
+        title: string,
+        artist: string
+    }[] = [];
 
     const songs = root.querySelectorAll('.lst50, .lst100');
     songs.forEach(song => {
         const title = song.querySelector('.ellipsis.rank01')?.text.trim()!;
-        chart.push(title);
+        const artist = song.querySelector('.ellipsis.rank02 a')?.text.trim()!;
+        chart.push({
+            title,
+            artist
+        });
     });
 
     if (chart.length === 0) return {
@@ -60,27 +69,59 @@ async function updatePlaylist(name: string): Promise<UpdateResult> {
         kv.set('latestmelonchart', chartHash)
     }
 
-    const cachedSearchResults = (await kv.hgetall('cachedsearchresults')) as Record<string, string> ?? {} // I know HGETALL is slow, but Vercel limits requests for KV so don't blame me :(
-    const resultsToCache: Record<string, string> = {}
+    // artists' names will be used to improve track search results
+    // I know HGETALL is slow, but Vercel limits requests for KV so don't blame me :(
+    const cachedKnownArtists: Record<string, string> = (await kv.hgetall('knownartists')) as Record<string, string> ?? {}
+    const artistSet = [...new Set(chart.map(song => song.artist))]; // using Set to remove duplicated values
 
+    const artistsToCache: Record<string, string> = {};
+    
+    await Promise.all(artistSet.map(async (artist) => {
+        if (cachedKnownArtists[artist]) return;
+        const results = await spotify.searchArtists(artist, { market: 'KR' })
+        const artistName = results.body.artists?.items[0]?.name;
+        if (artistName) artistsToCache[artist] = artistName;
+    }));
+
+    // cache artists
+    if (Object.keys(artistsToCache).length > 0) kv.hset('knownartists', artistsToCache)
+    kv.expire('knownartists', 60 * 60 * 24 * 7)
+    const artists = {...cachedKnownArtists, ...artistsToCache}
+
+    // search tracks
+    const cachedSearchResults = (await kv.hgetall('cachedsearchresults')) as Record<string, string> ?? {}
+    const resultsToCache: Record<string, string> = {}
     const trackURIs = await Promise.all(
-        chart.map(async (title): Promise<string> => {
-            if (cachedSearchResults[title]) return cachedSearchResults[title]
-            const results = await spotify.searchTracks(title, { market: 'KR' })
+        chart.map(async (song): Promise<string> => {
+            // use cached search results if possible
+            if (cachedSearchResults[song.title]) return cachedSearchResults[song.title]
+            if (artists[song.artist]) {
+                const results = await spotify.searchTracks(`track:${song.title} artist:${artists[song.artist]}`, { market: 'KR' })
+                if (results.body.tracks && results.body.tracks.items.length > 0) {
+                    const uri = results.body.tracks.items[0].uri
+                    resultsToCache[song.title] = uri
+                    return uri
+                } // if not found, it will use alternative search query without artist filter
+            }
+
+            // find track on spotify
+            const results = await spotify.searchTracks(song.title, { market: 'KR' })
 
             let track_uri = env.UNAVAILABLE_TRACK_URI
             if (results.body.tracks && results.body.tracks.items.length > 0) {
                 const uri = results.body.tracks.items[0].uri
                 track_uri = uri
-                resultsToCache[title] = uri
+                resultsToCache[song.title] = uri
             }
             return track_uri
         })
     )
     
+    // cache track search results
     if (Object.keys(resultsToCache).length > 0) kv.hset('cachedsearchresults', resultsToCache)
-    kv.expire('cachedsearchresults', 60 * 60 * 24 * 60)
+    kv.expire('cachedsearchresults', 60 * 60 * 24 * 3)
 
+    // replace tracks in playlist
     const maxRetries = 3
 
     for (let i = 0; i < maxRetries; i++) {
@@ -93,6 +134,7 @@ async function updatePlaylist(name: string): Promise<UpdateResult> {
         }
     }
 
+    // update last update data
     kv.hset('lastupdate', { at: getCurrentTimestamp(), by: name })
     const currentCount = await kv.zincrby('topupdaters', 1, name)
     return {
@@ -105,10 +147,15 @@ async function startUpdating(name: string) {
     setUpdatingState(true)
     let result: UpdateResult;
     try {
-        const timeoutPromise = new Promise<UpdateResult>(() => {return {
-            success: false,
-            message: '시간 초과!'
-        }});
+        // timeout after 30 seconds
+        const timeoutPromise = new Promise<UpdateResult>((resolve) => {
+            setTimeout(() => {
+                resolve({
+                    success: false,
+                    message: '시간 초과!'
+                });
+            }, 30000);
+        });
 
         result = await Promise.race([
             updatePlaylist(name),
@@ -128,6 +175,7 @@ export const GET: RequestHandler = async ({ request }) => {
     const url = new URL(request.url);
     const name = url.searchParams.get('name') ?? `익명${Math.floor(Math.random() * 100000000)}`;
     
+    // name validation    
     if (name.length < 1) return new Response(JSON.stringify({success: false, message: "이름은 최소 1자 이상이어야 합니다."}), {status: 400});
     if (name.length > 16) return new Response(JSON.stringify({success: false, message: "이름은 최대 16자 이하이어야 합니다."}), {status: 400});
 
@@ -136,13 +184,14 @@ export const GET: RequestHandler = async ({ request }) => {
         return new Response(JSON.stringify({success: false, message: "이름은 영문, 한글, 숫자만 포함해야 합니다."}), {status: 400});
     }
 
-    const blacklistedWords = ['도박', '카지노', '베팅'];
+    const blacklistedWords = ['도박', '카지노', '베팅']; // add as many as you want
     for (let word of blacklistedWords) {
         if (name.includes(word)) {
             return new Response(JSON.stringify({success: false, message: `이름에 금지된 단어 '${word}' 가 포함되어 있습니다.`}), {status: 400});
         }
     }
 
+    // check last update
     const fromLastCheck = getCurrentTimestamp() - Number(await kv.get('lastcheck'))
 
     const remainingTime = Math.floor(Number(env.UPDATE_CHECK_INTERVAL) - fromLastCheck)
