36dc588219ceae42c590cec59179f8beef13e76b
toast <toastlua@gmail.com>
2024-04-20 17:59:55 +0900

    Add comma to overrides.ts

--- src/lib/server/overrides.ts
+++ src/lib/server/overrides.ts
@@ -4,16 +4,16 @@ export const overrides: {
 } = {
     tracks: {
         // 여기의 트랙은 Spotify 검색 과정을 건너뛰고 대신 입력된 URI 값을 사용합니다.
-        // "멜론 트랙 이름 - 멜론 아티스트 이름": "스포티파이 URI"
+        // "멜론 트랙 이름 - 멜론 아티스트 이름": "스포티파이 URI",
 
         // ex)
-        // "홀씨 - 아이유": "spotify:track:0UTtK6hregIBOsefavRI26"
+        // "홀씨 - 아이유": "spotify:track:0UTtK6hregIBOsefavRI26",
 
         // 특정 트랙을 플레이리스트에서 없애려면 스포티파이 URI 대신 null 값을 할당합니다.
         // ex)
-        // "홀씨 - 아이유": null
+        // "홀씨 - 아이유": null,
         "그대만 있다면 (여름날 우리 X 너드커넥션 (Nerd Connection)) - 너드커넥션 (Nerd Connection)": "spotify:track:3TQHPUEVdvdq8ejwEcHUlL",
-        "파이팅 해야지 (Feat. 이영지) - 부석순 (SEVENTEEN)": "spotify:track:7eBpUuPnDTfbeP1P4P93CS"
+        "파이팅 해야지 (Feat. 이영지) - 부석순 (SEVENTEEN)": "spotify:track:7eBpUuPnDTfbeP1P4P93CS",
     },
     artists: {
         // 여기의 아티스트는 스포티파이 검색 과정을 건너뛰고 대신 여기에 할당된 아티스트 이름을 사용합니다.
@@ -25,6 +25,6 @@ export const overrides: {
         // 특정 아티스트의 트랙을 플레이리스트에서 없애려면 아티스트 이름 대신 null 값을 할당합니다.
         // ex)
         // "아이유": null
-        "아이유": "아이유"
+        "아이유": "아이유",
     }
 };
