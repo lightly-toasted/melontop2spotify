15e92a221de025de51355b4db604fb842dabdace
toast <toastlua@gmail.com>
2024-05-24 16:46:19 +0900

    Edit error page message

--- src/lib/server/spotify.ts
+++ src/lib/server/spotify.ts
@@ -3,6 +3,6 @@ import { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN } from '$env/static/private';
 
 export const spotify = new SpotifyWebApi({
     clientId: CLIENT_ID,
-	clientSecret: CLIENT_SECRET,
+    clientSecret: CLIENT_SECRET,
     refreshToken: REFRESH_TOKEN,
 })
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex items-center justify-center h-screen flex-col space-y-2 text-center">
     <h1 class="text-8xl font-black text-text">오류!! 😱</h1>
-    <p class="text-text">{$page.status} {$page.error?.message}... 하지만 적어도 우리에겐 고양이가 있어요 🐈🐈</p>
+    <p class="text-text">{$page.status} {$page.error?.message}... 고양이도 이해 못하는 페이지입니다ㅡㅡ!! 🐈🐈</p>
     <img alt="🐈" src="https://cataas.com/cat?width=384&height=224" class="my-2 w-96 min-h-56 bg-gradient-to-r from-yellow-200 to-orange-200" />
     <div class="flex-row">
         <a href="/" class="bg-primary rounded px-10 py-1 text-background">처음으로</a>
